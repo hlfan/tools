8b2b947f449b8389401eb4a41ada295681b52ebb
Marwin Hochfelsner <50826859+hlfan@users.noreply.github.com>
2024-11-14 19:52:07 +0100

    Create apple-token function

---
+++ netlify/functions/apple-token/apple-token.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+
+export default async (request) => {
+	let { next_run } = await request.json(),
+		tokenResponse = await fetch('https://maps.apple.com/place/mwtoken'),
+		token = await tokenResponse.text(),
+		headers = { authorization: `Bearer ${token}` },
+		bootstrapResponse = await fetch('https://cdn.apple-mapkit.com/ma/bootstrap', { headers }),
+		bootstrap = await bootstrapResponse.json(),
+		tileSourcesMapper = ({ tileSource, path, attributionId }) => [tileSource, { v: decodeURIComponent(path.match(/v=(\d+)/)[1]), attributionId }],
+		attributionsMapper = ({ attributionId, global }) => [attributionId, [...new Set(global.map(a => a.url))]],
+		out = {
+			accessKey: bootstrap.accessKey,
+			expiresAt: Date.now() + bootstrap.expiresInSeconds * 1000,
+			tiles: Object.fromEntries(bootstrap.tileSources.map(tileSourcesMapper)),
+			attributions: Object.fromEntries(bootstrap.attributions.map(attributionsMapper))
+		};
+
+	fs.writeFileSync('../../dist/apple-token.json', JSON.stringify(out));
+	console.log(`Cached token for until ${new Date(expiresAt)}. Next run at ${ ${new Date(next_run)}}`);
+};
+
+export const config = { schedule: "*/20 * * * *" };
