ce8418042d34d2be0b025c4cdea2df5a34afd752
Marwin Hochfelsner <50826859+hlfan@users.noreply.github.com>
2025-05-19 15:35:51 +0200

    Add initial page

---
+++ netlify/dist/bingleichda/app.js
@@ -0,0 +1,41 @@
+async function init () {
+	const response = await fetch("stations.json");
+	const stations = await response.json();
+
+	const sequences = getAllValidStationSequences(stations);
+	const randomSequence = sequences[Math.floor(Math.random() * sequences.length)];
+	renderSequence(randomSequence);
+}
+function getAllValidStationSequences (stations) {
+	function recurse (stack) {
+		const entries = stations[stack[0]];
+		if (!entries) return [[]];
+
+		return Object.entries(entries).flatMap(([line, stations]) =>
+			stations.flatMap(station =>
+				recurse(stack.slice(1)).map(rest =>
+					[
+						{
+							...station,
+							line
+						}, ...rest
+					]
+				)
+			)
+		);
+	}
+
+	const keys = Object.keys(stations);
+	return recurse(keys).filter(seq =>
+		new Set(seq.map(s => s.line)).size === keys.length
+	);
+}
+function renderSequence (seq) {
+	const ol = document.getElementById("sequence");
+	seq.forEach(({station, filename, line}) => {
+		const li = document.createElement("li");
+		li.innerHTML = `<span class="line">${line}</span>: ${station} <small>(${filename})</small>`;
+		ol.appendChild(li);
+	});
+}
+init();
